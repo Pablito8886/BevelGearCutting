058fea2fb4fd5a4db6fa05002f687d969cda99cd
pablito886 <Pawel.Trzewik88@gmail.com>
2018-01-22 22:21:11 +0100

    Read Input File Added

--- SymulacjaBevelGears/Program.cs
+++ SymulacjaBevelGears/Program.cs
@@ -2,13 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//using System.Threading.Tasks;
+using NXOpen;
+using NXOpen.UF;
+using NXOpenUI;
 
-namespace SymulacjaBevelGears
-{
+
     class Program
     {
-        static void Main(string[] args)
+
+        private static UI theUI;
+        public static Session theSession;
+        public static UFSession theUFSession;
+
+
+
+        public static void Main(string[] args)
         {
+            //Starting UG session
+            theSession = Session.GetSession();
+            theUFSession = UFSession.GetUFSession();
+
+            Part workPart = theSession.Parts.Work;
+
+            //open the LOG listening in NX
+            ListingWindow lw = theSession.ListingWindow;
+            lw.Open();
+
+            
+
+            //Calling a ReadFile class
+            ReadFile readFile = new ReadFile();
+            readFile.ReadData();
+
+            lw.WriteLine("Srednica glowicy frezowej: "+ readFile.d0);
+            lw.WriteLine("Sredni kat zarysu nozy glowicy frezowej: "+readFile.alf2);
+            lw.WriteLine("Szerokosc rozstawienia nozy glowicy frezowej: "+readFile.w2);
+            lw.WriteLine("Promien zaaokraglenia wierzcholka noza: "+readFile.r02);
+            lw.WriteLine("Kat zarysu noza na stronie wkleslej: "+readFile.alf2wk);
+            lw.WriteLine("Kat zarysu noza na stronie wypuklej: "+readFile.alf2wp);
+            lw.WriteLine("Ustawienie promieniowe glowicy: " + readFile.u);
+            lw.WriteLine("ustawienie katowe glowicy: " + readFile.q);
+            lw.WriteLine("liczba zebow: " + readFile.z);
+            lw.WriteLine("kierunek pochylenia linii zeba: " + readFile.kierunekLinii);
+            lw.WriteLine("dlugosc tworzacej stozka podzialowego: " + readFile.re);
+            lw.WriteLine("kat stozka podzialowego: " + readFile.del);
+            lw.WriteLine("kat stozka glow: " + readFile.delA);
+            lw.WriteLine("kat stozka stop: " + readFile.delF);
+            lw.WriteLine("szerokosc wienca zebatego: " + readFile.B);
+            lw.WriteLine("odleglosc wierzcholka stozka podzialowego od wierzcholkow glow: " + readFile.lwa);
+            lw.WriteLine("odleglosc wierzcholka stozka podzialowego od wierzcholkow stop: " + readFile.lwf);
+            lw.WriteLine("przesuniecie otoczki do plaszczyzny kolyski po osi otoczki : " + readFile.xp);
+            lw.WriteLine("przesuniecie otoczki do plaszczyzny kolyski w kierunku do niej normalnym : " + readFile.xb);
+            lw.WriteLine("powiekszenie otoczki : " + readFile.mat2);
+            lw.WriteLine("wysokosc glowy zeba : " + readFile.hae);
+            lw.WriteLine("wysokosc stopy zeba : " + readFile.hfe);
+            lw.WriteLine("jednostkowy kad symulacji oddtaczania : " + readFile.katIteracji);
+            lw.WriteLine("liczba krokow symulacji : " + readFile.kroki);
+            lw.WriteLine("przelozenie oddataczania : " + readFile.iOdt);
+            lw.WriteLine("przesuniecie hipoidalne : " + readFile.A);
+            lw.WriteLine("promien zaaokroglenia noza strony wkleslej : " + readFile.r02wk);
+            lw.WriteLine("promien zaaokroglenia noza strony wypuklej : " + readFile.r02wp);
+            lw.WriteLine("kat skrecenia wrzeciona przedmioto obrabianego : " + readFile.del2);
+            lw.WriteLine("obrot glowicy o kat J wynikajacy z TILT-u : " + readFile.tiltJ);
+            lw.WriteLine("obrot glowicy o kat I wynikajacy z TILT-u : " + readFile.tiltI);
+            lw.WriteLine("strona obrabiana ([1]-wypukla , [0]-wklesla : " + readFile.wyp);
+            lw.WriteLine("wspolczynnik modyfikacji oddtaczania : " + readFile.kM);
+            lw.WriteLine("wysokosc ostrzy glowicy : " + readFile.wys);
         }
+
+        public static int GetUnloadOption(string dummy) { return (int)Session.LibraryUnloadOption.Immediately; }
+        
     }
-}
+
---
+++ SymulacjaBevelGears/ReadFile.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NXOpen;
+using NXOpen.UF;
+using NXOpenUI;
+
+
+    class ReadFile
+    {
+        //Properites
+        public double d0 { get; set; }
+        public double alf2 { get; set; }
+        public double w2 { get; set; }
+        public double r02 { get; set; }
+        public double alf2wk { get; set; }
+        public double alf2wp { get; set; }
+        public double u { get; set; }
+        public double q { get; set; }
+        public double z { get; set; }
+        public double kierunekLinii { get; set; }
+        public double re { get; set; }
+        public double del { get; set; }
+        public double delA { get; set; }
+        public double delF { get; set; }
+        public double B { get; set; }
+        public double lwa { get; set; }
+        public double lwf { get; set; }
+        public double xp { get; set; }
+        public double xb { get; set; }
+        public double mat2 { get; set; }
+        public double hae { get; set; }
+        public double hfe { get; set; }
+        public double katIteracji { get; set; }
+        public double kroki { get; set; }
+        public double iOdt { get; set; }
+        public double A { get; set; }
+        public double r02wk { get; set; }
+        public double r02wp { get; set; }
+        public double del2 { get; set; }
+        public double tiltJ { get; set; }
+        public double tiltI { get; set; }
+        public double wyp { get; set; }
+        public double kM { get; set; }
+        public double wys { get; set; }
+
+
+
+
+        //---- DO EDYCJI ------//
+        static string directory = @"C:\PAWEL\Project\Bevel_Pattern_Check\Symulacja_NX_nowe\katalog_vs_2010\dane.txt";
+        //-------   -----------//
+
+
+        public void ReadData()
+        {
+            // Creating FileStream Objects
+            List<double> list = new List<double>();
+            FileStream fileStream = new FileStream(directory, FileMode.Open, FileAccess.Read);
+
+            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                string[] splitLines;
+                double dupa;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    splitLines = line.Split('=');
+                    MessageBox.Show(splitLines[1]);
+                    dupa = Double.Parse(splitLines[1].Replace(".",","));
+                    list.Add(dupa);
+                }
+            }
+
+            //Attach vales to properties
+            try
+            {
+                if (list.Count == 34)
+                {
+                    d0 = list[0];
+                    alf2 = list[1];
+                    w2 = list[2];
+                    r02 = list[3];
+                    alf2wk = list[4];
+                    alf2wp = list[5];
+                    u = list[6];
+                    q = list[7];
+                    z = list[8];
+                    kierunekLinii = list[9];
+                    re = list[10];
+                    del = list[11];
+                    delA = list[12];
+                    delF = list[13];
+                    B = list[14];
+                    lwa = list[15];
+                    lwf = list[16];
+                    xp = list[17];
+                    xb = list[18];
+                    mat2 = list[19];
+                    hae = list[20];
+                    hfe = list[21];
+                    katIteracji = list[22];
+                    kroki = list[23];
+                    iOdt = list[24];
+                    A = list[25];
+                    r02wk = list[26];
+                    r02wp = list[27];
+                    del2 = list[28];
+                    tiltI = list[29];
+                    tiltJ = list[30];
+                    wyp = list[31];
+                    kM = list[32];
+                    wys = list[33];
+                }
+                else { throw new IndexOutOfRangeException("Niepoprawna liczba argumentów w pliku inputowym"); }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+
+        }
+    }
+
