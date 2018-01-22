48a0e972ab6d1bb7f984e9aa39dbaca5e46abf7a
pablito886 <Pawel.Trzewik88@gmail.com>
2018-01-23 00:30:52 +0100

    Added Basic Geometry Class and Tool Geometry Class

---
+++ SymulacjaBevelGears/CordSystemAndGeometry.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NXOpen;
+using NXOpen.UF;
+using NXOpenUI;
+
+    class CordSystemAndGeometry
+    {
+
+        public Point Point3DCreation(Part workPart,double x,double y,double z)
+        {
+            Point3d originPointTemp = new Point3d(x, y, z);
+
+            Point originPoint = workPart.Points.CreatePoint(originPointTemp);
+            originPoint.SetVisibility(SmartObject.VisibilityOption.Visible);
+
+            return originPoint;
+        }
+
+        public DatumAxis DatumAxisCreation(Part workPart, Point first, Point second)
+        {
+            //Tworzenie "buildera dla osi Y".
+            NXOpen.Features.Feature nullFeatures_Feature = null;
+            NXOpen.Features.DatumAxisBuilder datumAxisBuilder;
+            datumAxisBuilder = workPart.Features.CreateDatumAxisBuilder(nullFeatures_Feature);
+
+            datumAxisBuilder.IsAssociative = true;
+            datumAxisBuilder.Type = NXOpen.Features.DatumAxisBuilder.Types.TwoPoints;
+            datumAxisBuilder.ResizedEndDistance = 0.0;
+
+            datumAxisBuilder.Point1 = first;
+            datumAxisBuilder.Point2 = second;
+
+            NXObject vector = datumAxisBuilder.Commit();
+
+            string journalName = vector.JournalIdentifier.ToString();
+            DatumAxis datumAxis = (DatumAxis)workPart.Datums.FindObject(journalName);
+
+            datumAxisBuilder.Destroy();
+
+            return datumAxis;
+        }
+
+
+        #region
+        public NXOpen.Features.DatumCsys CsysCreation(Part workPart,Point pointOrigin,Point pointAxis1,Point pointAxis2)
+        {
+            NXOpen.Features.Feature nullFeatures_Feature = null;
+            NXOpen.Features.DatumCsysBuilder datumCsysBuilder;
+            datumCsysBuilder = workPart.Features.CreateDatumCsysBuilder(nullFeatures_Feature);
+
+            Xform xform1;
+            xform1 = workPart.Xforms.CreateXform(pointOrigin, pointAxis1, pointAxis2, NXOpen.SmartObject.UpdateOption.WithinModeling, 1.0);
+
+            CartesianCoordinateSystem cartesianCoordinateSystem;
+            cartesianCoordinateSystem = workPart.CoordinateSystems.CreateCoordinateSystem(xform1, NXOpen.SmartObject.UpdateOption.WithinModeling);
+
+            datumCsysBuilder.Csys = cartesianCoordinateSystem;
+            datumCsysBuilder.ComponentsCreation = true;
+            datumCsysBuilder.FixedSizeDatum = true;
+            datumCsysBuilder.DisplayScaleFactor = 1.25;
+
+            NXObject cSys = datumCsysBuilder.Commit();
+
+            string journalName = cSys.JournalIdentifier.ToString();
+            NXOpen.Features.DatumCsys datumCsys = (NXOpen.Features.DatumCsys)workPart.Features.FindObject(journalName);
+
+            datumCsysBuilder.Destroy();
+
+            return datumCsys;
+        }
+
+        public NXOpen.Features.DatumCsys CsysCreation(Part workPart, Point pointOrigin, DatumAxis Axis1, DatumAxis Axis2)
+        {
+            NXOpen.Features.Feature nullFeatures_Feature = null;
+            NXOpen.Features.DatumCsysBuilder datumCsysBuilder;
+            datumCsysBuilder = workPart.Features.CreateDatumCsysBuilder(nullFeatures_Feature);
+            
+            Sense sense1=new Sense();
+            Sense sense2=new Sense();
+
+            Direction direction1;
+            direction1 = workPart.Directions.CreateDirection(Axis1,sense1,SmartObject.UpdateOption.WithinModeling);
+
+            Direction direction2;
+            direction2 = workPart.Directions.CreateDirection(Axis2, sense2, SmartObject.UpdateOption.WithinModeling);
+
+            Xform xform1;
+            xform1 = workPart.Xforms.CreateXform(pointOrigin, direction1, direction2, NXOpen.SmartObject.UpdateOption.WithinModeling, 1.0);
+
+            CartesianCoordinateSystem cartesianCoordinateSystem;
+            cartesianCoordinateSystem = workPart.CoordinateSystems.CreateCoordinateSystem(xform1, NXOpen.SmartObject.UpdateOption.WithinModeling);
+
+            datumCsysBuilder.Csys = cartesianCoordinateSystem;
+            datumCsysBuilder.ComponentsCreation = true;
+            datumCsysBuilder.FixedSizeDatum = true;
+            datumCsysBuilder.DisplayScaleFactor = 1.25;
+
+            NXObject cSys = datumCsysBuilder.Commit();
+
+            string journalName = cSys.JournalIdentifier.ToString();
+            NXOpen.Features.DatumCsys datumCsys = (NXOpen.Features.DatumCsys)workPart.Features.FindObject(journalName);
+
+            datumCsysBuilder.Destroy();
+
+            return datumCsys;
+        }
+        #endregion
+
+
+    }
+
+
+
---
+++ SymulacjaBevelGears/ToolGeometry.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NXOpen;
+using NXOpen.UF;
+using NXOpenUI;
+
+
+    class ToolGeometry
+    {
+
+        public void ToolSketch(Part workPart)
+        {
+            CordSystemAndGeometry geomObj = new CordSystemAndGeometry();
+
+            Point punkt0=geomObj.Point3DCreation(workPart, 0, 0, 0);
+            Point punkt1=geomObj.Point3DCreation(workPart, 10, 0, 0);
+            Point punkt2=geomObj.Point3DCreation(workPart, 0, 10, 0);
+            Point punkt3=geomObj.Point3DCreation(workPart, 0, 0, 10);
+            Point punkt4 = geomObj.Point3DCreation(workPart, 0, 10, 10);
+
+
+            DatumAxis axisX = geomObj.DatumAxisCreation(workPart, punkt0, punkt1);
+            DatumAxis axisY = geomObj.DatumAxisCreation(workPart, punkt0, punkt4);
+
+            NXOpen.Features.DatumCsys csys = geomObj.CsysCreation(workPart, punkt0, punkt1, punkt2);
+            NXOpen.Features.DatumCsys csys1 = geomObj.CsysCreation(workPart,punkt0,axisX,axisY);
+        }
+        
+        
+        
+        
+        // NXOpen.Features.DatumCsysBuilder datumCsysBuilder1;
+
+
+/*
+
+
+
+
+
+            Point3d origin111 = new Point3d(0.0, 0.0, 0.0);
+            Matrix3x3 matrix11;
+            matrix11.Xx = 1.0;
+            matrix11.Xy = 0.0;
+            matrix11.Xz = 0.0;
+            matrix11.Yx = 0.0;
+            matrix11.Yy = 1.0;
+            matrix11.Yz = 0.0;
+            matrix11.Zx = 0.0;
+            matrix11.Zy = 0.0;
+            matrix11.Zz = 1.0;
+            workPart.WCS.SetOriginAndMatrix(origin111, matrix11);
+
+
+        */
+    }
+
