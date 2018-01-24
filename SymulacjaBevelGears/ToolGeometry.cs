e1f088f3e492ad501ddcba987993e247a7481135
pablito886 <Pawel.Trzewik88@gmail.com>
2018-01-24 13:56:19 +0100

    Cutter Tool Body Feature - finished

--- SymulacjaBevelGears/ToolGeometry.cs
+++ SymulacjaBevelGears/ToolGeometry.cs
@@ -26,11 +26,14 @@ using NXOpenUI;
                 data.ReadData();
 
                 // Definition of cradle center point
-                Point ptCradleCenter = geomObj.Point3DCreation(workPart, 0, 0, 0);
+                //Point ptCradleCenter = geomObj.Point3DCreation(workPart, 0, 0, 0);
+                Point3d ptCradleCenter = new Point3d(0, 0, 0);
 
                 //Definition of cradle Csys
-                Point ptX = geomObj.Point3DCreation(workPart, 10, 0, 0);
-                Point ptY = geomObj.Point3DCreation(workPart, 0, 10, 0);
+               // Point ptX = geomObj.Point3DCreation(workPart, 10, 0, 0);
+               // Point ptY = geomObj.Point3DCreation(workPart, 0, 10, 0);
+                Point3d ptX = new Point3d(10, 0, 0);
+                Point3d ptY = new Point3d(0, 10, 0);
                 NXOpen.Features.DatumCsys csysCradle = geomObj.CsysCreation(workPart, ptCradleCenter, ptX, ptY);
 
                 //Definition of Cradle Angle sign
@@ -54,18 +57,19 @@ using NXOpenUI;
                 //Total tooth depth
                 double mat = data.hae + data.hfe;
                 //Starting points and line
-                Point pGlo1=geomObj.Point3DCreation(workPart, ptCradleCenter.Coordinates.X - data.wys, ptCradleCenter.Coordinates.Y + H, ptCradleCenter.Coordinates.Z + V);
-                Point pGlo2 = geomObj.Point3DPolar(workPart, pGlo1, -mat, 0);
+                Point3d pGlo1=new Point3d(ptCradleCenter.X - data.wys, ptCradleCenter.Y + H, ptCradleCenter.Z + V);
+                Point3d pGlo2 = geomObj.Point3DPolar(workPart, pGlo1, -mat, 0);
                 Line lGlo1 = geomObj.UnAssLines(workPart, pGlo1, pGlo2);
+
                 //cutter outline
-                Point pGlo8 = geomObj.Point3DPolar(workPart, pGlo2, data.d0 / 2, -90);
-                Point pGlo10 = geomObj.Point3DPolar(workPart, pGlo8, data.wys+mat, 0);
-                Point pGlo9 = geomObj.Point3DPolar(workPart, pGlo10, data.w2 / 2, -90);
-                Point pGlo5 = pGlo9;
-                Point pGlo4 = geomObj.Point3DPolar(workPart, pGlo5, data.wys / Math.Cos(data.alf2wk*Math.PI/180), 180+data.alf2wk);
-                Point pGlo3 = geomObj.Point3DPolar(workPart, pGlo4, mat, 180);
-                Point pGlo6 = geomObj.Point3DPolar(workPart, pGlo9, data.w2 / 2, 90);
-                Point pGlo7 = geomObj.Point3DPolar(workPart, pGlo6, data.wys / Math.Cos(data.alf2wp * Math.PI / 180), 180-data.alf2wp);
+                Point3d pGlo8 = geomObj.Point3DPolar(workPart, pGlo2, data.d0 / 2, -90);
+                Point3d pGlo10 = geomObj.Point3DPolar(workPart, pGlo8, data.wys+mat, 0);
+                Point3d pGlo9 = geomObj.Point3DPolar(workPart, pGlo10, data.w2 / 2, -90);
+                Point3d pGlo5 = pGlo9;
+                Point3d pGlo4 = geomObj.Point3DPolar(workPart, pGlo5, data.wys / Math.Cos(data.alf2wk*Math.PI/180), 180+data.alf2wk);
+                Point3d pGlo3 = geomObj.Point3DPolar(workPart, pGlo4, mat, 180);
+                Point3d pGlo6 = geomObj.Point3DPolar(workPart, pGlo9, data.w2 / 2, 90);
+                Point3d pGlo7 = geomObj.Point3DPolar(workPart, pGlo6, data.wys / Math.Cos(data.alf2wp * Math.PI / 180), 180-data.alf2wp);
                 Line lGlo2 = geomObj.UnAssLines(workPart, pGlo2, pGlo3);
                 Line lGlo3 = geomObj.UnAssLines(workPart, pGlo4, pGlo3);
                 Line lGlo4 = geomObj.UnAssLines(workPart, pGlo5, pGlo4);
@@ -73,55 +77,52 @@ using NXOpenUI;
                 Line lGlo6 = geomObj.UnAssLines(workPart, pGlo6, pGlo7);
                 Line lGlo7 = geomObj.UnAssLines(workPart, pGlo7, pGlo1);
 
-                Tag[] tagg=new Tag[2];
-                tagg[0]=lGlo7.Tag;
-                tagg[1]=lGlo6.Tag;
-                double[] center=new Double[3] {0,0,0};
-                int[] trims = new int[3];
-                trims[0] = 1;
-                trims[1] = 1;
-                trims[2] = 1;
-                int[] dupa = new int[2] { 0, 0 }; 
-                Tag fillet;
+                Point3d pointt = new Point3d(0, 0, 0);
+                //Point pointInput = workPart.Points.CreatePoint(pointt);
+                Point3d pointt2 = new Point3d(0, 20, 0);
+                //Point pointInput2 = workPart.Points.CreatePoint(pointt2);
 
-                UFSession theUfSession = UFSession.GetUFSession();
-                theUfSession.Curve.CreateFillet(0, tagg, center, 3, trims,dupa,out fillet);
+                Curve linia1 = workPart.Curves.CreateLine(pointt, pointt2);
+                linia1.SetVisibility(SmartObject.VisibilityOption.Visible);
 
-                NXOpen.TaggedObject taggedObject = NXOpen.Utilities.NXObjectManager.Get(fillet);
-                Arc arc = (Arc)taggedObject;
-                arc.Highlight();
+                //Sketch def
+                //Axis normal do sketch plane
+                Point3d helpPointN = new Point3d(0, 0, 1);
+                DatumAxis axisNor = geomObj.DatumAxisCreation(workPart, ptCradleCenter, helpPointN);
+                DatumPlane plane = geomObj.DatumPlaneCreation(workPart, pGlo2, axisNor);
 
+                //Axis perpendicular to the sketch plane
+                Point3d helpPointP = new Point3d(1, 0, 0);
+                DatumAxis axisPer = geomObj.DatumAxisCreation(workPart, ptCradleCenter, helpPointP);
+                Sketch sketchTool = geomObj.SketchCreation(workPart, theSession, pGlo2, axisPer, plane, csysCradle);
+
+                //Attach geometry to the sketch
+                sketchTool.AddGeometry(lGlo2);
+                sketchTool.AddGeometry(lGlo3);
+                sketchTool.AddGeometry(lGlo4);
+                sketchTool.AddGeometry(lGlo5);
+                sketchTool.AddGeometry(lGlo6);
+                sketchTool.AddGeometry(lGlo7);
+                sketchTool.Update();
+
+                //Cutter radius creation
+                        //Concave side
+                        Point3d helpPoint1 = geomObj.Point3dOnCurve(workPart, "0.5", lGlo5);
+                        Point3d helpPoint2 = geomObj.Point3dOnCurve(workPart, "0.5", lGlo4);
+                        Arc r02wk = geomObj.FilletOnSketch(theSession, lGlo5, lGlo4, helpPoint1, helpPoint2, data.r02wk);
+                        //Convex side
+                        helpPoint1 = geomObj.Point3dOnCurve(workPart, "0.5", lGlo5);
+                        helpPoint2 = geomObj.Point3dOnCurve(workPart, "0.5", lGlo6);
+                        Arc r02wp = geomObj.FilletOnSketch(theSession, lGlo5, lGlo6, helpPoint1, helpPoint2, data.r02wp);
+                 sketchTool.Update();
+
+                //Creation a solid cutter
+                        //Axis def
+                        Vector3d vectorCutterAxis=new Vector3d(1,0,0);
+                        Axis axisCutter = geomObj.AxisCreation(workPart, pGlo1, vectorCutterAxis);
+                        Body cutter=geomObj.Revolve(workPart, axisCutter, sketchTool, lGlo6, pGlo1,0,360);
             }
             catch (Exception ex) { MessageBox.Show(ex.ToString());}
-
         }
-        
-        
-        
-        
-        // NXOpen.Features.DatumCsysBuilder datumCsysBuilder1;
-
-
-/*
-
-
-
-
-
-            Point3d origin111 = new Point3d(0.0, 0.0, 0.0);
-            Matrix3x3 matrix11;
-            matrix11.Xx = 1.0;
-            matrix11.Xy = 0.0;
-            matrix11.Xz = 0.0;
-            matrix11.Yx = 0.0;
-            matrix11.Yy = 1.0;
-            matrix11.Yz = 0.0;
-            matrix11.Zx = 0.0;
-            matrix11.Zy = 0.0;
-            matrix11.Zz = 1.0;
-            workPart.WCS.SetOriginAndMatrix(origin111, matrix11);
-
-
-        */
     }
 
