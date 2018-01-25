62471f138a637182d1327bb42a131368d3d8852e
pablito886 <Pawel.Trzewik88@gmail.com>
2018-01-25 16:16:29 +0100

    Created cutting simulation class

---
+++ SymulacjaBevelGears/CuttingSimulation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NXOpen;
+using NXOpen.UF;
+using NXOpenUI;
+
+
+    class CuttingSimulation
+    {
+        public void CuttingSGM(Part workPart, Session theSession)
+        {
+            ToolGeometry glowica = new ToolGeometry();
+            Body cutter=glowica.ToolSketch(workPart, theSession);
+
+            BlankGeometry otoczka = new BlankGeometry();
+            Body bodyBlank=otoczka.BlankGeometryCreation(workPart, theSession);
+        }
+    }
+
